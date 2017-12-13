cfcdb1802cccd19da56d6fb66196c308d66a4aee
ekitselyuk <ekitselyuk@ronasit.com>
2017-12-13 12:45:41 +0600

    #-: fix exceptions render

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,8 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,10 +45,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if ($exception instanceof NotFoundHttpException) {
+        if ($exception instanceof HttpException) {
             return response()->json(
                 ['error' => $exception->getMessage()],
-                Response::HTTP_NOT_FOUND
+                $exception->getStatusCode()
             );
         }
 
