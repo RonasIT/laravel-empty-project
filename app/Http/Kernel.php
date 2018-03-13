ce97dd5e596c994d7ada2fce5e130b4d4aa7322a
Roman Dubrovin <rdubrovin@ronasit.com>
2018-03-13 12:49:54 +0600

    #-: fixed jwt.auth in Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'auth:api' => GetUserFromToken::class
+        'jwt.auth' => GetUserFromToken::class
     ];
 }
