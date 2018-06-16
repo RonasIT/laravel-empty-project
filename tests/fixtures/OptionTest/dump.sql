6ed469ea88183f50203f041784b0ccd69cbd3ec2
evikulov <evikulov@ronasit.com>
2018-06-17 00:06:15 +0600

    #6: add options

---
+++ tests/fixtures/OptionTest/dump.sql
@@ -0,0 +1,5 @@
+INSERT INTO options(key, value, created_at, updated_at) VALUES
+  ('attribute', '{"name": "color", "value":"#000"}'::json, '2016-10-20 11:05:00', '2016-10-20 11:06:00'),
+  ('settings', '{"timezone": "australia"}'::json, '2016-10-20 11:06:00', '2016-10-20 11:07:00'),
+  ('mailgun', '{"api_key": "superKey", "account_id":"3495"}'::json, '2016-10-20 11:07:00', '2016-10-20 11:08:00'),
+  ('states', '["NSW", "ACT", "NT", "QLD", "SA", "TAS", "VIC", "WA"]'::json, '2016-10-20 11:08:00', '2016-10-20 11:09:00');
