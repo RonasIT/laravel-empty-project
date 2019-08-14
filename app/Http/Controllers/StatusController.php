419db439e1d9cbec43d46b8117c52fed737105e1
Evgeniy Korniushin <ekorniushin@ronasit.com>
2019-08-14 06:22:29 +0000

    #19: "Создать status страничку"

---
+++ app/Http/Controllers/StatusController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use Exception;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller as BaseController;
+
+class StatusController extends BaseController
+{
+    public function status()
+    {
+        try {
+            DB::getPdo();
+        } catch (Exception $e) {
+            return response('', Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+
+        return response('', Response::HTTP_OK);
+    }
+}
