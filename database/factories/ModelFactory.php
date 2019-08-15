f685e727e5c58da6af751b806ba81df189a07401
Mikhail Ronzhin <mronzhin@ronasit.com>
2019-08-15 09:40:53 +0000

    #20: "Обновить Laravel до топовой версии"

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -19,7 +21,7 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'remember_token' => Str::random(10),
     ];
 });
 
