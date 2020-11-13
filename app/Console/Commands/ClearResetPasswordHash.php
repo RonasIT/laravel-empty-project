cead37f5765c8456272f1455de14525fb1376d3c
Adil Ausagit <6kdoter@gmail.com>
2020-11-13 18:52:29 +0600

    #29 refactored artisan command, maybe need to update with service

--- app/Console/Commands/ClearResetPasswordHash.php
+++ app/Console/Commands/ClearResetPasswordHash.php
@@ -7,43 +7,14 @@ use Illuminate\Console\Command;
 
 class ClearResetPasswordHash extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'clear:reset-password-hash';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Clear reset_password_hash in users table';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
     public function handle()
     {
-        if (!empty((User::first()))) {
-            User::query()->update(['reset_password_hash' => '']);
-            $this->line('Reset password hash was cleared');
+        User::query()->update(['reset_password_hash' => null]);
 
-            return;
-        }
-        $this->error('Reset password hash was not cleared');
+        $this->line('Reset password hash was cleared');
     }
 }
