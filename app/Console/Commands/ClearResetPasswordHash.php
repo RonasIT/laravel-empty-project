dd1446c995c643508499a18d412307c72c163759
Adil Ausagit <6kdoter@gmail.com>
2020-11-13 15:46:54 +0600

    #29 clear reset password hash command added

---
+++ app/Console/Commands/ClearResetPasswordHash.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class ClearResetPasswordHash extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clear:reset_password_hash';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear reset_password_hash in users table';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if (!empty((User::first()))) {
+            User::query()->update(['reset_password_hash' => '']);
+            $this->line('Reset password hash was cleared');
+
+            return;
+        }
+        $this->error('Reset password hash was not cleared');
+    }
+}
