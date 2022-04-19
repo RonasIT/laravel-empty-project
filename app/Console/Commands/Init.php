e1c05ae88fe4649ec35834065fab3c636f1831ec
dpankratov <dpankratov@ronasit.com>
2022-04-19 12:55:05 +0600

    feat: update init command
    - removed dot env parsing logic;
    - use the same method as for update yml files;
    - removed yaml parser library;

--- app/Console/Commands/Init.php
+++ app/Console/Commands/Init.php
@@ -18,46 +18,26 @@ class Init extends Command
         $appName = $this->ask('Please set application name');
         $kebabName = Str::kebab($appName);
 
-        $this->updateDotEnv('.env.testing', [
+        $this->updateConfigFile('.env.testing', '=', [
             'APP_NAME' => $appName,
             'DATA_COLLECTOR_KEY' => $kebabName
         ]);
 
-        $this->updateDotEnv('.env', [
+        $this->updateConfigFile('.env', '=', [
             'APP_NAME' => $appName,
             'DATA_COLLECTOR_KEY' => $kebabName
         ]);
 
-        $this->updateGitLabCiYaml($appName, $kebabName);
+        $this->updateConfigFile('.gitlab-ci.yml', ': ', [
+            'CI_PROJECT_NAME' => $kebabName,
+            'DOMAIN' => "{$kebabName}.ronasit.com",
+            'APP_NAME' => $appName,
+            'DATA_COLLECTOR_KEY' => $kebabName
+        ]);
 
         if ($this->confirm('Do you want generate admin user?', true)) {
             $this->createAdminUser("admin@{$kebabName}.com");
         }
-    }
-
-    protected function updateDotEnv(string $fileName, array $data)
-    {
-        $dotEnv = $this->parseDotEnv($fileName);
-
-        foreach ($dotEnv as &$block) {
-            $keysToUpdate = array_intersect_key($data, $block);
-
-            $block = array_merge($block, $keysToUpdate);
-        }
-
-        $this->dumpDotEnv($dotEnv, $fileName);
-    }
-
-    protected function updateGitLabCiYaml($appName, $kebabName)
-    {
-        $ymlSettings = file_get_contents('.gitlab-ci.yml');
-
-        $ymlSettings = Str::replaceFirst('CI_PROJECT_NAME: laravel', "CI_PROJECT_NAME: {$kebabName}", $ymlSettings);
-        $ymlSettings = Str::replaceFirst('DOMAIN: laravel.ronasit.com', "DOMAIN: {$kebabName}.ronasit.com", $ymlSettings);
-        $ymlSettings = Str::replaceFirst('APP_NAME: Laravel', "APP_NAME: \"{$appName}\"", $ymlSettings);
-        $ymlSettings = Str::replaceFirst('DATA_COLLECTOR_KEY: laravel', "DATA_COLLECTOR_KEY: {$kebabName}", $ymlSettings);
-
-        file_put_contents('.gitlab-ci.yml', $ymlSettings);
     }
 
     protected function createAdminUser($defaultEmail)
@@ -72,61 +52,38 @@ class Init extends Command
         ]);
     }
 
-    protected function parseDotEnv($fileName): array
+    protected function addQuotes($string): string
     {
-        $dotEnvContent = file_get_contents($fileName);
-
-        $lines = explode("\n", $dotEnvContent);
-
-        $result = [[]];
-
-        $lastLineIndex = array_key_last($lines);
-
-        foreach ($lines as $index => $line) {
-            if (!empty($line)) {
-                $exploded = explode('=', $line);
-
-                $block = array_pop($result);
-
-                $block[array_shift($exploded)] = implode('=', $exploded);
-
-                $result[] = $block;
-            } elseif ($index !== $lastLineIndex) {
-                $result[] = [];
-            }
-        }
-
-        return $result;
+        return (Str::contains($string, ' ')) ? "\"{$string}\"" : $string;
     }
 
-    protected function dumpDotEnv(array $parsed, $fileName)
-    {
-        $result = '';
-
-        $lastBlockIndex = array_key_last($parsed);
-
-        foreach ($parsed as $index => $block) {
-            foreach ($block as $key => $value) {
-                if (Str::contains($value, ' ')) {
-                    $value = "\"{$value}\"";
-                }
-
-                $result .= "{$key}={$value}\n";
-            }
-
-            if ($index !== $lastBlockIndex) {
-                $result .= "\n";
-            }
-        }
-
-        return file_put_contents($fileName, $result);
-    }
-
-    public function publishMigration($admin)
+    protected function publishMigration($admin)
     {
         $data = view('add_default_user')->with($admin)->render();
         $fileName = Carbon::now()->format('Y_m_d_His') . '_add_default_user.php';
 
         return file_put_contents("database/migrations/{$fileName}", "<?php\n\n{$data}");
+    }
+
+    protected function updateConfigFile($fileName, $separator, $data)
+    {
+        $parsed = file_get_contents($fileName);
+
+        $lines = explode("\n", $parsed);
+
+        foreach ($lines as &$line) {
+            foreach ($data as $key => $value) {
+                if (Str::contains($line, "{$key}{$separator}")) {
+                    $exploded = explode($separator, $line);
+                    $key = array_shift($exploded);
+                    $value = $this->addQuotes($value);
+                    $line = "{$key}{$separator}{$value}";
+                }
+            }
+        }
+
+        $ymlSettings = implode("\n", $lines);
+
+        file_put_contents($fileName, $ymlSettings);
     }
 }
