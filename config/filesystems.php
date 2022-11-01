db41870fad6c1c4a7b9c3a3919e3b3411bf533d0
roman2027@internet.ru <roman2027@internet.ru>
2022-11-01 11:48:32 +0600

    chore: change comment Refs: #8

--- config/filesystems.php
+++ config/filesystems.php
@@ -22,9 +22,9 @@ return [
     |
     | Here you may configure as many filesystem "disks" as you wish, and you
     | may even configure multiple disks of the same driver. Defaults have
-    | been set up for each driver as an example of the required options.
+    | been set up for each driver as an example of the required values.
     |
-    | Supported Drivers: "local", "ftp", "s3", "rackspace"
+    | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
     */
 
