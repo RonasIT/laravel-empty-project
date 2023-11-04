141284ccde247bba0a7a036c2ae0f49e10c0ed57
Vitaly Iskrin <viskrin@ronasit.com>
2023-11-03 22:16:03 -0500

    test: cover InitCommand

--- tests/InitCommandTest.php
+++ tests/InitCommandTest.php
@@ -13,7 +13,7 @@ class InitCommandTest extends TestCase
         TestCase::setUp();
     }
 
-    public function testRunWithoutAdminCreation()
+    public function testRunWithoutAdminAndReadmeCreation()
     {
         $filePutContentsMock = $this->getFunctionMock('App\Console\Commands', 'file_put_contents');
 
@@ -23,16 +23,18 @@ class InitCommandTest extends TestCase
                 ['.env.testing', $this->getFixture('env.testing.yml')],
                 ['.env', $this->getFixture('env.yml')],
                 ['.env.development', $this->getFixture('env.development.yml')],
-                ['.env.ci-testing', $this->getFixture('env.ci-testing.yml')]
+                ['.env.ci-testing', $this->getFixture('env.ci-testing.yml')],
             );
 
-        $this->artisan('init my-app')
-            ->expectsOutput('Project initialized successfully')
-            ->expectsConfirmation('Do you want generate admin user?', 'no')
+        $this->artisan('init "My App"')
+            ->expectsOutput('Project initialized successfully!')
+            ->expectsQuestion('Please enter an application URL', 'https://mysite.com')
+            ->expectsConfirmation('Do you want to generate an admin user?')
+            ->expectsConfirmation('Do you want to generate a README file?')
             ->assertExitCode(0);
     }
 
-    public function testRunWithAdminCreation()
+    public function testRunWithAdminAndWithoutReadmeCreation()
     {
         $filePutContentsMock = $this->getFunctionMock('App\Console\Commands', 'file_put_contents');
 
@@ -43,15 +45,117 @@ class InitCommandTest extends TestCase
                 ['.env', $this->getFixture('env.yml')],
                 ['.env.development', $this->getFixture('env.development.yml')],
                 ['.env.ci-testing', $this->getFixture('env.ci-testing.yml')],
-                ['database/migrations/2018_11_11_111111_add_default_user.php', $this->getFixture('migration.php')]
+                ['database/migrations/2018_11_11_111111_add_default_user.php', $this->getFixture('migration.php')],
             );
 
-        $this->artisan('init my-app')
-            ->expectsOutput('Project initialized successfully')
-            ->expectsConfirmation('Do you want generate admin user?', 'yes')
-            ->expectsQuestion('Please enter admin name', 'TestAdmin')
-            ->expectsQuestion('Please enter admin email', 'mail@mail.com')
-            ->expectsQuestion('Please enter admin password', '123456')
+        $this->artisan('init "My App"')
+            ->expectsOutput('Project initialized successfully!')
+            ->expectsQuestion('Please enter an application URL', 'https://mysite.com')
+            ->expectsConfirmation('Do you want to generate an admin user?', 'yes')
+            ->expectsQuestion('Please enter an admin name', 'TestAdmin')
+            ->expectsQuestion('Please enter an admin email', 'mail@mail.com')
+            ->expectsQuestion('Please enter an admin password', '123456')
+            ->expectsConfirmation('Do you want to generate a README file?')
+            ->assertExitCode(0);
+    }
+
+    public function testRunWithAdminAndDefaultReadmeCreation()
+    {
+        $filePutContentsMock = $this->getFunctionMock('App\Console\Commands', 'file_put_contents');
+
+        $filePutContentsMock
+            ->expects($this->exactly(6))
+            ->withConsecutive(
+                ['.env.testing', $this->getFixture('env.testing.yml')],
+                ['.env', $this->getFixture('env.yml')],
+                ['.env.development', $this->getFixture('env.development.yml')],
+                ['.env.ci-testing', $this->getFixture('env.ci-testing.yml')],
+                ['database/migrations/2018_11_11_111111_add_default_user.php', $this->getFixture('migration.php')],
+                ['README.md', $this->getFixture('default_readme.md')],
+            );
+
+        $this->artisan('init "My App"')
+            ->expectsOutput('Project initialized successfully!')
+            ->expectsQuestion('Please enter an application URL', 'https://mysite.com')
+            ->expectsConfirmation('Do you want to generate an admin user?', 'yes')
+            ->expectsQuestion('Please enter an admin name', 'TestAdmin')
+            ->expectsQuestion('Please enter an admin email', 'mail@mail.com')
+            ->expectsQuestion('Please enter an admin password', '123456')
+            ->expectsConfirmation('Do you want to generate a README file?', 'yes')
+            ->expectsConfirmation('Do you need a `Resources & Contacts` part?', 'yes')
+            ->expectsConfirmation('Are you going to use Issue Tracker?', 'yes')
+            ->expectsQuestion('Please enter a Issue Tracker link', '')
+            ->expectsConfirmation('Are you going to use Figma?', 'yes')
+            ->expectsQuestion('Please enter a Figma link', '')
+            ->expectsConfirmation('Are you going to use Sentry?', 'yes')
+            ->expectsQuestion('Please enter a Sentry link', '')
+            ->expectsConfirmation('Are you going to use DataDog?', 'yes')
+            ->expectsQuestion('Please enter a DataDog link', '')
+            ->expectsConfirmation('Are you going to use ArgoCD?', 'yes')
+            ->expectsQuestion('Please enter a ArgoCD link', '')
+            ->expectsConfirmation('Are you going to use Laravel Telescope?', 'yes')
+            ->expectsQuestion('Please enter a Laravel Telescope link', '')
+            ->expectsQuestion('Please enter a Manager contact', '')
+            ->expectsQuestion('Please enter a Code Owner/Team Lead contact', '')
+            ->expectsConfirmation('Do you need a `Prerequisites` part?', 'yes')
+            ->expectsConfirmation('Do you need a `Getting Started` part?', 'yes')
+            ->expectsConfirmation('Do you need an `Environments` part?', 'yes')
+            ->expectsConfirmation('Do you need a `Credentials and Access` part?', 'yes')
+            ->expectsOutput('README generated successfully!')
+            ->expectsOutput('Don`t forget to fill the following empty values:')
+            ->expectsOutput('- Issue Tracker link')
+            ->expectsOutput('- Figma link')
+            ->expectsOutput('- Sentry link')
+            ->expectsOutput('- DataDog link')
+            ->expectsOutput('- ArgoCD link')
+            ->expectsOutput('- Manager contact')
+            ->expectsOutput('- Code Owner/Team Lead contact')
+            ->assertExitCode(0);
+    }
+
+    public function testRunWithAdminAndFullReadmeCreation()
+    {
+        $filePutContentsMock = $this->getFunctionMock('App\Console\Commands', 'file_put_contents');
+
+        $filePutContentsMock
+            ->expects($this->exactly(6))
+            ->withConsecutive(
+                ['.env.testing', $this->getFixture('env.testing.yml')],
+                ['.env', $this->getFixture('env.yml')],
+                ['.env.development', $this->getFixture('env.development.yml')],
+                ['.env.ci-testing', $this->getFixture('env.ci-testing.yml')],
+                ['database/migrations/2018_11_11_111111_add_default_user.php', $this->getFixture('migration.php')],
+                ['README.md', $this->getFixture('full_readme.md')],
+            );
+
+        $this->artisan('init "My App"')
+            ->expectsOutput('Project initialized successfully!')
+            ->expectsQuestion('Please enter an application URL', 'https://mysite.com')
+            ->expectsConfirmation('Do you want to generate an admin user?', 'yes')
+            ->expectsQuestion('Please enter an admin name', 'TestAdmin')
+            ->expectsQuestion('Please enter an admin email', 'mail@mail.com')
+            ->expectsQuestion('Please enter an admin password', '123456')
+            ->expectsConfirmation('Do you want to generate a README file?', 'yes')
+            ->expectsConfirmation('Do you need a `Resources & Contacts` part?', 'yes')
+            ->expectsConfirmation('Are you going to use Issue Tracker?', 'yes')
+            ->expectsQuestion('Please enter a Issue Tracker link', 'https://gitlab.com/my-project')
+            ->expectsConfirmation('Are you going to use Figma?', 'yes')
+            ->expectsQuestion('Please enter a Figma link', 'https://figma.com/my-project')
+            ->expectsConfirmation('Are you going to use Sentry?', 'yes')
+            ->expectsQuestion('Please enter a Sentry link', 'https://sentry.com/my-project')
+            ->expectsConfirmation('Are you going to use DataDog?', 'yes')
+            ->expectsQuestion('Please enter a DataDog link', 'https://datadoghq.com/my-project')
+            ->expectsConfirmation('Are you going to use ArgoCD?', 'yes')
+            ->expectsQuestion('Please enter a ArgoCD link', 'https://argocd.com/my-project')
+            ->expectsConfirmation('Are you going to use Laravel Telescope?', 'yes')
+            ->expectsQuestion('Please enter a Laravel Telescope link', 'https://mypsite.com/telescope-link')
+            ->expectsQuestion('Please enter a Manager contact', 'manager@mail.com')
+            ->expectsQuestion('Please enter a Code Owner/Team Lead contact', 'lead@mail.com')
+            ->expectsConfirmation('Do you need a `Prerequisites` part?', 'yes')
+            ->expectsConfirmation('Do you need a `Getting Started` part?', 'yes')
+            ->expectsConfirmation('Do you need an `Environments` part?', 'yes')
+            ->expectsConfirmation('Do you need a `Credentials and Access` part?', 'yes')
+            ->expectsOutput('README generated successfully!')
             ->assertExitCode(0);
     }
 }
---
+++ tests/fixtures/InitCommandTest/full_readme.md
@@ -0,0 +1,52 @@
+# My App
+
+Project description will be here
+
+## Project Resources & Contacts
+This section provides quick links to various resources and contacts associated
+with this project. It's here to streamline your navigation and communication
+process, so you can efficiently find what you need or reach out to who you need.
+
+### Resources
+Below are links to tools and services used in this project:
+- Issue Tracker: Here, you can report any issues or bugs related to the project. [Issue Tracker](https://gitlab.com/my-project)
+- Figma: This is where we maintain all our design assets and mock-ups. [Figma](https://figma.com/my-project)
+- Sentry: To monitor application performance and error tracking. [Sentry](https://sentry.com/my-project)
+- DataDog: This is where we monitor our logs, and server performance, and receive alerts. [DataDog](https://datadoghq.com/my-project)
+- ArgoCD: Is a kubernetes controller which continuously monitors running applications. [ArgoCD](https://argocd.com/my-project)
+- Laravel Telescope: This is debug assistant for the Laravel framework. [Laravel Telescope](https://mypsite.com/telescope-link)
+- [API Documentation](https://mysite.com)
+
+### Contacts
+Should you need assistance or have questions, feel free to connect with the following individuals:
+- Manager: If you have any high-level project concerns, feel free to get in touch with our project manager. [Connect with Manager](manager@mail.com)
+- Code Owner/Team Lead: For specific questions about the codebase or technical aspects, reach out to our team lead. [Connect with Team Lead](lead@mail.com)
+Please be mindful of each individual's preferred contact method and office hours.
+
+## Prerequisites
+To work with this repository, you will need to have the following
+installed:
+- [Docker](https://www.docker.com)
+
+## Getting Started
+To get started with this repository, follow these steps:
+Clone this repository to your local machine.
+```sh
+git clone git@github.com:RonasIT/laravel-empty-project.git
+```
+Build and start containers. It may takes some time.
+```sh
+docker compose up -d
+```
+
+## Environments
+This repository by default supports three environments: `local`, `development`,
+and `testing`. Each environment is represented by an appropriate environment file:
+| Environment | File | URL                                  |
+| --- | --- |--------------------------------------|
+| local | .env | [http://localhost](http://localhost) |
+| testing | .env.testing | -                                    |
+| development | .env.development | [https://mysite.com](https://mysite.com)               |
+
+## Credentials and Access
+Default admin email and password: `mail@mail.com`/`123456`
