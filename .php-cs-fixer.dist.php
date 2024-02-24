3cfe358c65f245888c5d9598bdbe20406111e8b0
Evgeny Ulin <eulin@ronasit.com>
2024-02-24 19:42:44 +0300

    #43: Linter and formatter packages and their settings have been updated.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,7 +2,7 @@
 
 $finder = PhpCsFixer\Finder::create()
     ->exclude(['docker', 'k8s', 'vendor', 'bootstrap/cache'])
-    ->notPath(['_ide_helper.php', '_ide_helper_models.php'])
+    ->notPath(['_ide_helper.php', '_ide_helper_models.php', 'tests/fixtures/InitCommandTest/migration.php'])
     ->in(__DIR__);
 
 $config = new PhpCsFixer\Config();
@@ -11,5 +11,13 @@ return $config
     ->setRules([
         '@PSR12' => true,
         'array_syntax' => ['syntax' => 'short'],
+        'type_declaration_spaces' => [
+            'elements' => ['function', 'property'],
+        ],
+        'no_extra_blank_lines' => true,
+        'blank_line_before_statement' => [
+            'statements' => ['return'],
+        ],
+        'trailing_comma_in_multiline' => true,
     ])
     ->setFinder($finder);
