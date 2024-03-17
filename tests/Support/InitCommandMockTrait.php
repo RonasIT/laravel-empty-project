98767ed0a017816866104ce743bcac2f39c28ec9
dpankratov <dpankratov@ronasit.com>
2024-03-17 17:45:08 +0600

    fix: test mocking

--- tests/Support/InitCommandMockTrait.php
+++ tests/Support/InitCommandMockTrait.php
@@ -15,10 +15,8 @@ trait InitCommandMockTrait
         $mock
             ->expects($this->exactly(4 + count($arguments)))
             ->withConsecutive(
-                ['.env.testing', $this->getFixture('env.testing.yml')],
                 ['.env.example', $this->getFixture('env.example.yml')],
                 ['.env.development', $this->getFixture('env.development.yml')],
-                ['.env.ci-testing', $this->getFixture('env.ci-testing.yml')],
                 ...$arguments
             );
     }
