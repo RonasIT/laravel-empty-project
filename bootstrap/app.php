a384fa37be4c474b18bc8ff7e09ac64fa43ae0bb
Alexey Vardugin <avardugin@ronasit.com>
2024-08-21 13:13:34 +0600

    feat: move middlewares setup to bootastrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,16 +1,32 @@
 <?php
 
+use App\Http\Middleware\CheckVersionMiddleware;
 use App\Http\Middleware\ClearVersion;
+use App\Http\Middleware\EncryptCookies;
+use App\Http\Middleware\RedirectIfAuthenticated;
+use App\Http\Middleware\TrimStrings;
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
+use Illuminate\Auth\Middleware\Authorize;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode;
+use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
+use Illuminate\Foundation\Http\Middleware\ValidatePostSize;
+use Illuminate\Http\Middleware\HandleCors;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Session\TokenMismatchException;
 use Illuminate\Validation\ValidationException;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use RonasIT\Support\AutoDoc\Http\Middleware\AutoDocMiddleware;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -26,17 +42,49 @@ return Application::configure(basePath: dirname(__DIR__))
         // TODO: need to check how it works, maybe this row is not necessary
         $middleware->throttleApi('60,1');
 
-        $middleware->group('auth_group', [
-            Authenticate::class,
+        $middleware->use([
+            HandleCors::class,
             CheckForMaintenanceMode::class,
-        ]);
-
-        $middleware->group('guest_group', [
-            CheckForMaintenanceMode::class,
+            ValidatePostSize::class,
+            TrimStrings::class,
+            ConvertEmptyStringsToNull::class,
+            AutoDocMiddleware::class,
+            CheckVersionMiddleware::class,
         ]);
 
         $middleware->alias([
+            'auth' => Authenticate::class,
+            'auth.basic' => AuthenticateWithBasicAuth::class,
+            'bindings' => SubstituteBindings::class,
+            'can' => Authorize::class,
+            'guest' => RedirectIfAuthenticated::class,
+            'throttle' => ThrottleRequests::class,
+            'maintenance' => CheckForMaintenanceMode::class,
             'clear_version' => ClearVersion::class,
+        ]);
+
+        $middleware->group('web', [
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
+            //AuthenticateSession::class,
+            ShareErrorsFromSession::class,
+            VerifyCsrfToken::class,
+            SubstituteBindings::class,
+        ]);
+
+        $middleware->group('api', [
+            'throttle:60,1',
+            'bindings',
+        ]);
+
+        $middleware->group('auth_group', [
+            'auth',
+            'maintenance',
+        ]);
+
+        $middleware->group('guest_group', [
+            'maintenance',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
