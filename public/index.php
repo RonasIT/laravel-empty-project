9e5dd25e56835500a1bd7926ced214f939498b1e
DenTray <dpankratov@ronasit.com>
2024-08-23 18:56:08 +0600

    Apply suggestions from code review

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php'
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
