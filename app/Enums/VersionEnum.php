d37520ba51ec70f678171e74dc6ab9f386987b4a
dpankratov <dpankratov@ronasit.com>
2025-07-02 10:09:20 +0600

    feat: use new Laravel helpers version

--- app/Enums/VersionEnum.php
+++ app/Enums/VersionEnum.php
@@ -2,11 +2,17 @@
 
 namespace App\Enums;
 
+use RonasIT\Support\Contracts\VersionEnumContract;
 use RonasIT\Support\Traits\EnumTrait;
 
-enum VersionEnum: string
+enum VersionEnum: string implements VersionEnumContract
 {
     use EnumTrait;
 
     case v0_1 = '0.1';
+
+    public static function last(): VersionEnum
+    {
+        return self::v0_1;
+    }
 }
